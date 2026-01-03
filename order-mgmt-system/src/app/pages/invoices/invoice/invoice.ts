f1b74d77e7631cc687a4f07eff7a6f6e3944bc1e
QuantumSoham <quantumsc03@gmail.com>
2026-01-03 14:45:15 +0530

    added logic for invoice component

--- order-mgmt-system/src/app/pages/invoices/invoice/invoice.ts
+++ order-mgmt-system/src/app/pages/invoices/invoice/invoice.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BillingService } from '../../../core/services/billing';
 
 @Component({
-  selector: 'app-invoice',
-  imports: [],
-  templateUrl: './invoice.html',
-  styleUrl: './invoice.css',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './invoice.html'
 })
-export class Invoice {
+export class InvoicesComponent implements OnInit {
 
+  userId = 2; // 🔒 hardcoded for now
+  invoices: any[] = [];
+  loading = false;
+
+  constructor(private billingService: BillingService) {}
+
+  ngOnInit() {
+    this.loadInvoices();
+  }
+
+  loadInvoices() {
+    this.loading = true;
+
+    this.billingService.getOrdersByUser(this.userId)
+      .subscribe(orders => {
+
+        orders.forEach(order => {
+          this.billingService.getInvoiceByOrder(order.orderId)
+            .subscribe(invoice => {
+              this.invoices.push(invoice);
+            });
+        });
+
+        this.loading = false;
+      });
+  }
+
+  pay(invoiceId: number) {
+    this.billingService.payInvoice(invoiceId)
+      .subscribe({
+        next: () => {
+          alert('Payment successful');
+          this.invoices = [];
+          this.loadInvoices();
+        },
+        error: err => {
+          console.error(err);
+          alert('Payment failed');
+        }
+      });
+  }
 }
