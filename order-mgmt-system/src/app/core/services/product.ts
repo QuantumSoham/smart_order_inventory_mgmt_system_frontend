1ae5793680006223b508118cff20c1552e2ef401
QuantumSoham <quantumsc03@gmail.com>
2026-01-06 12:41:56 +0530

    product component

--- order-mgmt-system/src/app/core/services/product.ts
+++ order-mgmt-system/src/app/core/services/product.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class ProductService {
 
-  private base = `http://localhost:8051`;
-
-  getProduct(id: number) {
-    return this.http.get<Product>(`${this.base}/products/${id}`);
-  }
+  private baseUrl = `http://localhost:8051/products`;
 
   constructor(private http: HttpClient) {}
+  getProduct(id: number) {
+    return this.http.get<Product>(`${this.baseUrl}/${id}`);
+  }
+
+  getAll(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
+  }
+
+  getById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.baseUrl}/${id}`);
+  }
+
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product);
+  }
+
+  update(id: number, product: Product): Observable<Product> {
+    return this.http.put<Product>(`${this.baseUrl}/${id}`, product);
+  }
+
+  delete(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+
 }
 
